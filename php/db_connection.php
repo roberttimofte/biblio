34fcd9c00493a1e002db0a890e173fc95a9f5807
Robert Timofte <roberttimofte15@gmail.com>
2018-04-16 13:10:28 +0200

    php

---
+++ php/db_connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "biblio";
+
+// Create connection
+$connessione = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$connessione) {
+    die("Connessione fallita: " . mysqli_connect_error());
+}
+?>
